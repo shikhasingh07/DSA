ec0df08e335260a3cfc4d02ab2eb6b6ced2ac6de
shikhasingh07 <shikha.thakur2295@gmail.com>
2026-04-07 21:40:50 +0530

    Happy Number

---
+++ ArrayHashMap/202.js
@@ -0,0 +1,21 @@
+var isHappy = function (n) {
+  if (n <= 0) {
+    return false;
+  }
+
+  let set = new Set();
+  while (n !== 1) {
+    if (set.has(n)) return false;
+    set.add(n);
+    let sum = 0;
+    while (n > 0) {
+      let nums = n % 10;
+      sum += nums * nums;
+      n = Math.floor(n / 10);
+    }
+    n = sum;
+  }
+  return true;
+};
+
+console.log(isHappy(2));
