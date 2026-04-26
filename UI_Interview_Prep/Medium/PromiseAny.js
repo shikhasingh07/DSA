3c7c8990017b5de73fd605c75d540d91bc19fe96
shikhasingh07 <shikha.thakur2295@gmail.com>
2026-04-26 23:16:40 +0530

    Promise.any

---
+++ UI_Interview_Prep/Medium/PromiseAny.js
@@ -0,0 +1,28 @@
+function promiseAny(iterable) {
+  let counter = 0;
+  let error = [];
+  return new Promise((res, rej) => {
+    if (iterable.length === 0) rej(new AggregateError([]));
+
+    iterable.forEach((element) => {
+      Promise.resolve(element)
+        .then(res)
+        .catch((err) => {
+          counter++;
+          error.push(err);
+          if (counter === iterable.length) {
+            rej(new AggregateError(error));
+          }
+        });
+    });
+  });
+}
+
+const p0 = Promise.resolve(42);
+const p1 = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    resolve(21);
+  }, 100);
+});
+
+console.log(await promiseAny([p0, p1]));
